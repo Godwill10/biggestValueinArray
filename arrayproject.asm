1d393c7087425c6134ea7bade92a8d97d07ae48d
Godwill10 <88179621+Godwill10@users.noreply.github.com>
2022-05-02 11:26:00 -0500

    Removed Description

--- arrayproject.asm
+++ arrayproject.asm
@@ -1,21 +1,4 @@
 
-# Author: Godwill Afolabi
-# arrayproject.asm
-#
-# Creates a function which finds the largest value in an array of integers
-#
-# int largest( int* array, int arrayLength) {
-#    int largest = array[0];
-#    int index = 1;
-#    while( index < arrayLength) {
-#        if( array[index] > largest {
-#            largest = array[index];
-#        }
-#        index++;
-#    }
-#    return( largest);
-#}
-#
 
 .data
   array1:  .word   10, 49, -3, 52, 187, 88
